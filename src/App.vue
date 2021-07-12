5b44ead26deeb983bb275765c98cd238dade3448
Yoichi Kobayashi <info@tplh.net>
2021-07-13 08:55:20 +0900

    drawing facemesh results on canvas2D,

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,31 @@ div
 import { defineComponent } from 'vue'
 import 'normalize.css'
 import { Camera } from '@mediapipe/camera_utils'
-// import drawingUtils from '@mediapipe/drawing_utils'
+import drawingUtils from '@mediapipe/drawing_utils'
 import { FaceMesh } from '@mediapipe/face_mesh'
+import * as mpFaceMesh from '@mediapipe/face_mesh'
 
 export default defineComponent({
   name: 'App',
   mounted() {
     const video = document.getElementById('input-video')
+    const canvas = document.getElementById('output-canvas')
+    const canvasCtx = canvas.getContext('2d')
     const camera = new Camera(video, {
       onFrame: () => {
+        if (video.videoWidth === 0) return
+        const aspect = video.videoHeight / video.videoWidth
+        let width = 0
+        let height = 0
+        if (window.innerWidth > window.innerHeight) {
+          height = video.videoHeight
+          width = height / aspect
+        } else {
+          width = video.videoWidth
+          height = width * aspect
+        }
+        canvas.width = width
+        canvas.height = height
         faceMesh.send({
           image: video,
         })
@@ -30,7 +46,56 @@ export default defineComponent({
 
     camera.start()
     faceMesh.onResults((results) => {
-      console.log(results)
+      canvasCtx.save()
+      canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
+      canvasCtx.drawImage(results.image, 0, 0, canvas.width, canvas.height)
+      if (results.multiFaceLandmarks) {
+        for (const landmarks of results.multiFaceLandmarks) {
+          drawingUtils.drawConnectors(
+            canvasCtx,
+            landmarks,
+            mpFaceMesh.FACEMESH_TESSELATION,
+            { color: '#C0C0C070', lineWidth: 1 }
+          )
+          drawingUtils.drawConnectors(
+            canvasCtx,
+            landmarks,
+            mpFaceMesh.FACEMESH_RIGHT_EYE,
+            { color: '#FF3030' }
+          )
+          drawingUtils.drawConnectors(
+            canvasCtx,
+            landmarks,
+            mpFaceMesh.FACEMESH_RIGHT_EYEBROW,
+            { color: '#FF3030' }
+          )
+          drawingUtils.drawConnectors(
+            canvasCtx,
+            landmarks,
+            mpFaceMesh.FACEMESH_LEFT_EYE,
+            { color: '#30FF30' }
+          )
+          drawingUtils.drawConnectors(
+            canvasCtx,
+            landmarks,
+            mpFaceMesh.FACEMESH_LEFT_EYEBROW,
+            { color: '#30FF30' }
+          )
+          drawingUtils.drawConnectors(
+            canvasCtx,
+            landmarks,
+            mpFaceMesh.FACEMESH_FACE_OVAL,
+            { color: '#E0E0E0' }
+          )
+          drawingUtils.drawConnectors(
+            canvasCtx,
+            landmarks,
+            mpFaceMesh.FACEMESH_LIPS,
+            { color: '#E0E0E0' }
+          )
+        }
+      }
+      canvasCtx.restore()
     })
   },
 })
@@ -38,6 +103,7 @@ export default defineComponent({
 
 <style lang="scss">
 video {
+  display: none;
   position: absolute;
   top: 0;
   bottom: 0;
